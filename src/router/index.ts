09f950b1bc01911ca32bd855b62362cd80abe417
Davron Usmonov <akunin94@gmail.com>
2023-01-16 22:56:55 +0500

    Fix deploy to gh-pages

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import {createRouter, createWebHistory} from 'vue-router';
+import {createRouter, createWebHashHistory} from 'vue-router';
 
 import Cart from '@/views/Cart.vue'
 import Main from '@/views/Main.vue'
@@ -18,7 +18,7 @@ const routes = [
 
 const router = createRouter({
   routes, 
-  history: createWebHistory()
+  history: createWebHashHistory()
 })
 
 export default router
